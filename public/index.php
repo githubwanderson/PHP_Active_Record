9b063cd926a0b7de120d193f7502bac30745a70c
Wanderson <wanderson.martins@magis5.com.br>
2024-11-24 17:04:45 -0300

    Criado a classe User e a classe abstrata ActiveRecord

--- public/index.php
+++ public/index.php
@@ -2,3 +2,10 @@
 
 require '../vendor/autoload.php';
 
+use app\database\models\User;
+
+$user = new User;
+$user->firstName = "Wanderson";
+$user->lastName = "Martins";
+$user->id = 1;
+// var_dump($user);
