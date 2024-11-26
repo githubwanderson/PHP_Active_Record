4ebd004e08d5587d6b6299771f54c80212c8369d
Wanderson <emailwandersonalves@gmail.com>
2024-11-26 07:57:42 -0300

    Implementado classe ActiveRecord e Update com interfaces

--- public/index.php
+++ public/index.php
@@ -3,9 +3,12 @@
 require '../vendor/autoload.php';
 
 use app\database\models\User;
+use app\database\activerecord\Update;
 
 $user = new User;
 $user->firstName = "Wanderson";
 $user->lastName = "Martins";
 $user->id = 1;
-// var_dump($user);
+var_dump($user);
+
+$user->update(new Update);
