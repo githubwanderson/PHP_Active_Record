86a52edb17d8dddaee9bf34182deb48f130def61
Wanderson <wanderson.martins@magis5.com.br>
2024-12-03 08:34:04 -0300

    Implementado a classe Delete

---
+++ app/database/activerecord/Delete.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace app\database\activerecord;
+
+use app\database\interfaces\ActiveRecordInterface;
+use app\database\interfaces\ActiveRecordExecuteInterface;
+use Throwable;
+use app\database\connection\Connection;
+
+class Delete implements ActiveRecordExecuteInterface
+{
+    private $field;
+    private $value;
+
+    public function __construct($field, $value) {
+        $this->field = $field;
+        $this->value = $value;
+    }
+
+    public function execute(ActiveRecordInterface $activeRecordInterface)
+    {
+        try {
+            $query = $this->createQuery($activeRecordInterface);
+            $connection = Connection::connect();
+            $prepare = $connection->prepare($query);
+            $prepare->execute([
+                $this->field => $this->value
+            ]);
+            return $prepare->rowCount();          
+        } catch (Throwable $th) {
+            formatException($th);
+        }
+    }
+
+    private function createQuery(ActiveRecordInterface $activeRecordInterface)
+    {
+        $sql = "DELETE FROM {$activeRecordInterface->getTable()}";
+        $sql .= " WHERE {$this->field} = :{$this->field}";
+        return $sql;     
+    }
+}
